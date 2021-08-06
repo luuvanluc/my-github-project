d782cd9a244b0761d03831a281062877a1bae7e3
luclv <luc.luuvan@gmail.com>
2021-08-06 09:37:11 +0700

    ++ Add test case for utest

--- unittest-win32-project/UnittestHinhChuNhat/test.cpp
+++ unittest-win32-project/UnittestHinhChuNhat/test.cpp
@@ -2,6 +2,24 @@
 #include "HinhChuNhat.h"
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST(HinhChuNhat, tinhDienTich)
+{
+	HinhChuNhat hcn;
+	hcn.setChieuDai(3);
+	hcn.setChieuRong(2);
+	float dienTich = hcn.tinhDienTich();
+	EXPECT_FLOAT_EQ(dienTich, 6);
+}
+
+TEST(HinhChuNhat, tinhChuVi)
+{
+	HinhChuNhat hcn;
+	hcn.setChieuDai(3);
+	hcn.setChieuRong(2);
+	float dienTich = hcn.tinhChuVi();
+	EXPECT_FLOAT_EQ(dienTich, 10);
 }
